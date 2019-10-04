ffb52308f393592b287182bb4295b9db7dd81e03
imbradyboy <parent.brady@gmail.com>
2019-10-04 18:41:34 -0400

    added mongodb - lesson 5

--- app.js
+++ app.js
@@ -6,8 +6,25 @@ var logger = require('morgan');
 
 var indexRouter = require('./controllers/index');
 var usersRouter = require('./controllers/users');
+//add new tasks controller (Lesson 5)
+const tasksRouter = require('./controllers/tasks');
 
 var app = express();
+
+//db
+const mongoose = require('mongoose');
+const globals = require('./config/globals');
+
+mongoose.connect(globals.db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(
+    (res) => {
+      console.log("Connected to MongoDB");
+    }
+).catch(() => {
+  console.log('Connection error');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +38,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//map any url's with "/tasks" to the task controller
+app.use('/tasks', tasksRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ config/globals.js
@@ -0,0 +1,4 @@
+//set up public array of global variables
+module.exports = {
+    'db' : 'mongodb+srv://admin:Password1@cluster0-ugkjh.azure.mongodb.net/tasks'
+};
